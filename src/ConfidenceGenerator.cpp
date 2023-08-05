2fc4d748a94c04db7a4f76f2a4ec35eb93926010
Chance Cuddeback <chancecuddeback@gmail.com>
2023-08-04 17:24:54 -0700

    started ConfidenceGenerator

--- src/ConfidenceGenerator.cpp
+++ src/ConfidenceGenerator.cpp
@@ -1,2 +1,49 @@
 #include "ConfidenceGenerator.h"
+#include <cmath>
 
+ConfidenceGenerator::ConfidenceGenerator(float alpha)
+{
+    _avg_step.reserve(100);
+    _avg_step_sigma.reserve(100);
+    _alpha = alpha;
+}
+
+
+float ConfidenceGenerator::update_confidence(float sig, float phase)
+{   
+    const int floored_phase = static_cast<int>(floor(phase)); // Only works for an array size of 100
+    // Compare to prior phase pair
+    const float prior = _lerp(_avg_step[floored_phase], _avg_step[floored_phase+1], phase-floored_phase);
+    const float delta = sig - prior;
+    const float percent_error = abs(delta/prior)*100;
+    // Conditionally add the new pair (weighted update)
+    if (_should_update(percent_error))
+    {
+        const float new_value = _ema(sig, prior, _alpha);
+        _update(floored_phase, phase, new_value, delta);
+    }
+    // Compute the confidence
+    return percent_error;
+}
+
+bool _should_update(float percent_error)
+{
+    return true;
+}
+
+void _update(int i, float phase, float new_value, float delta)
+{
+
+}
+
+template <typename T>
+T _lerp(T a, T b, T t)
+{
+    return a + t * (b - a);
+}
+
+template <typename T>
+T _ema(T new_value, T old_value, T alpha)
+{
+    return old_value + alpha*(new_value-old_value);
+}
--- src/ConfidenceGenerator.h
+++ src/ConfidenceGenerator.h
@@ -6,11 +6,12 @@
  * @brief Uses a signal[Sig] (FSR, Angle, ect) and percent gait phase to generate percent confidence. This simply correlates Sig and gait phase data to generate 
  * trajectories (Sig vs phase). If the current (Sig, Phase) is different from past (Sig, Phase) pairs the confidence will be lower. 
  */
+#include <vector>
 
 
 class ConfidenceGenerator {
     public:
-    ConfidenceGenerator();
+    ConfidenceGenerator(float alpha);
 
     /**
      * @brief Given a signal and phase pair, this function returns how similar this pair is to prior step pairs. 
@@ -24,7 +25,52 @@ class ConfidenceGenerator {
 
 
     private:
+    std::vector<float> _avg_step; // All of the steps averaged into one
+    std::vector<float> _avg_step_sigma; // The symetric variance of the average step
+    float _alpha;
 
+    /**
+     * @brief If the new value should be added to the step average
+     * 
+     * @param percent_error 
+     * @return true 
+     * @return false 
+     */
+    bool _should_update(float percent_error);
+
+    /**
+     * @brief Update the average step
+     * 
+     * @param i 
+     * @param phase 
+     * @param new_value 
+     * @param delta
+     */
+    void _update(int i, float phase, float new_value, float delta);
+
+    /**
+     * @brief Linear interpolation
+     * 
+     * @tparam T 
+     * @param a 
+     * @param b 
+     * @param t 
+     * @return T 
+     */
+    template <typename T>
+    T _lerp(T a, T b, T t);
+
+    /**
+     * @brief Exponentially weighted moving average.
+     * 
+     * @tparam T 
+     * @param new_value 
+     * @param old_value 
+     * @param alpha 
+     * @return T 
+     */
+    template <typename T>
+    T _ema(T new_value, T old_value, T alpha);
 };
 
 
